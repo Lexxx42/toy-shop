f42a9fe5e8c9807d39a18441672e4cb23d3e620e
Alexandr Konukhov <jesusmisha@gmail.com>
2023-03-01 16:19:28 +0300

    added toys

--- toyshop/Main.java
+++ toyshop/Main.java
@@ -1,9 +1,15 @@
 package toyshop;
+
 import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) {
         Toy toy1 = new Toy("1 2 конструктор");
+        Toy toy2 = new Toy("2 2 робот");
+        Toy toy3 = new Toy("3 6 кукла");
+
         System.out.println(toy1.getIdToy());
+        System.out.println(toy1.getDropToy());
+        System.out.println(toy1.getNameToy());
     }
 }
--- toyshop/Toy.java
+++ toyshop/Toy.java
@@ -1,14 +1,28 @@
 package toyshop;
 
 class Toy {
-    private final String idToy;
+    private final Integer idToy;
+    private final Integer dropToy;
+    private final String nameToy;
     private String inputData;
 
     public Toy(String inputData) {
-        this.idToy = inputData;
+        String[] data = inputData.split(" ");
+        this.idToy = Integer.parseInt(data[0]);
+        this.dropToy = Integer.parseInt(data[1]);
+        this.nameToy = data[2];
     }
-    public String getIdToy() {
+
+    public Integer getIdToy() {
         return idToy;
+    }
+
+    public Integer getDropToy() {
+        return dropToy;
+    }
+
+    public String getNameToy() {
+        return nameToy;
     }
 
 }
